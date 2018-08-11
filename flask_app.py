fd63b83c8a679571196cdbcb09dacff19ba5153d
Helder Rezende <helderfgv@gmail.com>
2018-08-10 21:51:22 -0300

    feat: sslify and facebook

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect
+from flask_sslify import SSLify
 import pandas as pd
 
 
 app = Flask(__name__)
+#sslify = SSLify(app)
 
 @app.route('/')
 def index():
